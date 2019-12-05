a3efb125a6e24b7d9d839ddff7ff0c578d63cd47
Tomas Nunes <tomas.costa.nunes@gmail.com>
2019-12-05 02:55:04 +0000

    Provide getters for unexported variables of City and Alien and export create methods. Add output file option.

--- cmd/invasion/main.go
+++ cmd/invasion/main.go
@@ -21,14 +21,15 @@ func printUsage() {
 		"invasion - This program reads and constructs world X, simulates an alien invasion and prints the final state of the world.\n" +
 		"\n" +
 		"Usage:\n" +
-		"%s [-h] [N] [FILE]\n" +
+		"%s [-h] [N] [INPUT_FILE] [OUTPUT_FILE]\n" +
 		"\n" +
 		"Flags:\n" +
-		"-h\tPrints this message.\n" +
+		"-h\t\tPrints this message.\n" +
 		"\n" +
 		"Args:\n" +
-		"N\tNumber of alien invaders, if none provided defaults to %d.\n" +
-		"FILE\tName of the input file with the world map description, if none provided defaults to '%s'.\n",
+		"N\t\tNumber of alien invaders, if none provided defaults to %d.\n" +
+		"INPUT_FILE\tName of the input file with the world map description, if none provided defaults to '%s'.\n" +
+        "OUTPUT_FILE\tName of the output file to create and print program information, if none provided defaults to the stdout.\n",
 		os.Args[0], defaultNumberAliens, defaultInputFile)
 }
 
@@ -42,38 +43,51 @@ func main() {
 	}
 
 	args := os.Args
+	totalArgs := len(args)
 	var numberAliens int
-	if len(args) > 1 {
+	if totalArgs > 1 {
 		var err error
 		if numberAliens, err = strconv.Atoi(args[1]); err != nil {
-			printUsage()
-			log.Fatal(err)
+			defer printUsage()
+			log.Panic(err)
 		}
 	} else {
 		numberAliens = defaultNumberAliens
 	}
 
 	var filename string
-	if len(args) > 2 {
+	if totalArgs > 2 {
 		filename = args[2]
 	} else {
 		filename = defaultInputFile
 	}
 
+	var writer *bufio.Writer
+	if totalArgs > 3 {
+        if outputFile, err := os.Create(args[3]); err != nil {
+            defer printUsage()
+            log.Panic(err)
+        } else {
+            writer = bufio.NewWriter(outputFile)
+        }
+    } else {
+        writer = bufio.NewWriter(os.Stdout)
+    }
+
 	if info, err := os.Stat(filename); err != nil {
-		printUsage()
-		log.Fatal(err)
+		defer printUsage()
+		log.Panic(err)
 	} else if info.IsDir() {
-		printUsage()
-		log.Fatalf("%s: is a directory, should be a file with the description of the world map.", filename)
+		defer printUsage()
+		log.Panicf("%s is a directory, should be a file with the description of the world map.", filename)
 	}
 
-	Invade(filename, numberAliens)
+	Invade(filename, numberAliens, writer)
 }
 
 // Creates world map with the description on the file, generates aliens, runs the simulation of the invasion,
-// and prints the final state of the world
-func Invade(filename string, numberAliens int) {
+// and prints the final state of the world.
+func Invade(filename string, numberAliens int, writer *bufio.Writer) {
     file, err := os.Open(filename)
     if err != nil {
         log.Panic(err)
@@ -89,7 +103,12 @@ func Invade(filename string, numberAliens int) {
     scanner := bufio.NewScanner(file)
     world.ReadWorldMap(scanner)
     world.GenerateAliens(numberAliens)
-    world.RunSimulation()
+    world.RunSimulation(writer)
 
-    fmt.Print(world.String())
+    if _, err := fmt.Fprint(writer, world.String()); err != nil {
+        log.Panic(err)
+    }
+    if err := writer.Flush(); err != nil {
+        log.Panic(err)
+    }
 }
--- pkg/worldx/worldx.go
+++ pkg/worldx/worldx.go
@@ -11,12 +11,12 @@ import (
 )
 
 type WorldX struct {
-    cities map[string]*city  // Maps city name to pointer of respective city
-    aliens map[string]*alien // Maps alien name to pointer of respective alien
+    Cities map[string]*City  // Maps city name to pointer of respective city
+    Aliens map[string]*Alien // Maps alien name to pointer of respective alien
 }
 
 func (w *WorldX) String() (wStr string) {
-    for _, c := range w.cities {
+    for _, c := range w.Cities {
         wStr += c.String() + "\n"
     }
 
@@ -29,23 +29,23 @@ func (w *WorldX) ReadWorldMap(scanner *bufio.Scanner) {
     const defaultDirectionSeparator string = "="
 
     for scanner.Scan() {
-        var newCity *city
+        var newCity *City
         cityDetails := strings.Fields(scanner.Text())
 
         for i, value := range cityDetails {
             if i == 0 {
-                newCity = w.createCity(value)
+                newCity = w.CreateCity(value)
             } else if newCity != nil {
                 directionDetails := strings.SplitN(value, defaultDirectionSeparator, 2)
-                dir := getDirection(directionDetails[0])
+                dir := GetDirection(directionDetails[0])
 
                 // Ignore directions without city name, with empty city name, and invalid directions
-                if len(directionDetails) == 2 && len(directionDetails[1]) > 0 && dir.isValid() {
+                if len(directionDetails) == 2 && len(directionDetails[1]) > 0 && dir.IsValid() {
 
                     // Only add connection if it doesn't exist yet, duplicated connections are ignored
                     if newCity.connectedCities[dir] == nil {
-                        connectedCity := w.createCity(directionDetails[1])
-                        w.createConnection(newCity, connectedCity, dir)
+                        connectedCity := w.CreateCity(directionDetails[1])
+                        w.AddConnection(newCity, connectedCity, dir)
                     }
                 }
             }
@@ -60,16 +60,18 @@ func (w *WorldX) ReadWorldMap(scanner *bufio.Scanner) {
 // Generates aliens one at a time placing them in a random empty city.
 // Panics on the tentative to generate more aliens than the number of cities.
 func (w *WorldX) GenerateAliens(numberAliens int) {
-    if totalAliens, totalCities := len(w.aliens) + numberAliens, len(w.cities); numberAliens <= 0 {
+    if totalAliens, totalCities := len(w.Aliens) + numberAliens, len(w.Cities); numberAliens < 0 {
         log.Panicf("GenerateAliens: the number of aliens to be generated need to be positive.")
     } else if totalAliens > totalCities {
         log.Panicf(
             "GenerateAliens: cannot have more aliens in the world than the number of cities! aliens: %d > cities: %d",
             totalAliens, totalCities)
+    } else if numberAliens == 0 {
+        return
     }
 
-    emptyCities := make([]string, 0, len(w.cities))
-    for name, c := range w.cities {
+    emptyCities := make([]string, 0, len(w.Cities))
+    for name, c := range w.Cities {
         if c.alien == nil {
             emptyCities = append(emptyCities, name)
         }
@@ -78,57 +80,73 @@ func (w *WorldX) GenerateAliens(numberAliens int) {
     rand.Seed(time.Now().UnixNano())
     for alienIndex := 0; alienIndex < numberAliens; alienIndex++ {
         alienName := strconv.Itoa(alienIndex)
-        w.createAlien(alienName, emptyCities)
+        w.CreateAlien(alienName, emptyCities)
     }
 }
 
-// Simulates invasion moving each alien defaultMaxIterations times and destroying city if aliens collide.
-func (w *WorldX) RunSimulation() {
+// Simulates invasion moving each alien `defaultMaxIterations` times or until it's trapped in an isolated city.
+// When two aliens meet in the same city they fight and in the process, both aliens die and the city is destroyed
+// severing all its connections. Prints message to the writer for every city destroyed.
+func (w *WorldX) RunSimulation(writer *bufio.Writer) {
     const defaultMaxIterations int = 10000
 
     rand.Seed(time.Now().UnixNano())
     for iteration := 0; iteration < defaultMaxIterations; iteration++ {
-        for _, a := range w.aliens {
-            w.moveAlien(a)
+        for _, a := range w.Aliens {
+            w.moveAlien(a, writer)
         }
+    }
+
+    if err := writer.Flush(); err != nil {
+        log.Panic(err)
     }
 }
 
-// Creates and adds city to the world if it doesn't exist yet, returns pointer to city with requested name
-func (w *WorldX) createCity(cityName string) *city {
-    if w.cities == nil {
-        w.cities = make(map[string]*city)
+// Creates and adds city to the world if it doesn't exist yet, returns pointer to city with requested name.
+func (w *WorldX) CreateCity(cityName string) *City {
+    if w.Cities == nil {
+        w.Cities = make(map[string]*City)
     }
 
-    if c, ok := w.cities[cityName]; ok {
+    if c, ok := w.Cities[cityName]; ok {
         return c
     } else {
-        newCity := city{
+        newCity := City{
             name:            cityName,
-            connectedCities: [maxDirections]*city{},
+            connectedCities: [MaxDirections]*City{},
             alien:           nil,
         }
-        w.cities[cityName] = &newCity
+        w.Cities[cityName] = &newCity
         return &newCity
     }
 }
 
-// Add bidirectional connection between city1 and city2, panics if any of the cities doesn't exist
-func (w *WorldX) createConnection(city1 *city, city2 *city, dir direction) {
+// Add bidirectional connection between city1 and city2.
+// Panics if any of the cities doesn't exist or the direction isn't valid.
+func (w *WorldX) AddConnection(city1 *City, city2 *City, dir Direction) {
     if city1 == nil || city2 == nil {
-        log.Panicln("createConnection: city doesn't exist, cannot create connection if city is nil.")
+        log.Panicln("AddConnection: city doesn't exist, cannot create connection if either city is <nil>.")
+    } else if !dir.IsValid() {
+        log.Printf("AddConnection: invalid direction %v.", dir)
     }
 
     city1.connectedCities[dir] = city2
-    city2.connectedCities[dir.getOpposite()] = city1
+    city2.connectedCities[dir.GetOpposite()] = city1
+
+    if city1.alien != nil && city1.alien.isTrapped {
+        city1.alien.isTrapped = false
+    }
+    if city2.alien != nil && city2.alien.isTrapped {
+        city2.alien.isTrapped = false
+    }
 }
 
 // Returns pointer to random city without an alien.
-// Empty cities slice should contain at least one empty city, otherwise enters an infinite loop
-func (w *WorldX) getRandomEmptyCity(emptyCities []string) (randomEmptyCity *city) {
+// Empty cities slice should contain at least one empty city, otherwise enters an infinite loop.
+func (w *WorldX) getRandomEmptyCity(emptyCities []string) (randomEmptyCity *City) {
     totalEmptyCities := len(emptyCities)
     for {
-        randomEmptyCity = w.cities[emptyCities[rand.Intn(totalEmptyCities)]]
+        randomEmptyCity = w.Cities[emptyCities[rand.Intn(totalEmptyCities)]]
 
         if randomEmptyCity.alien == nil {
             return
@@ -136,31 +154,31 @@ func (w *WorldX) getRandomEmptyCity(emptyCities []string) (randomEmptyCity *city
     }
 }
 
-// If alien doesn't exist, creates it in a random empty city and adds it to the world
-// Returns pointer to alien with requested name
-func (w *WorldX) createAlien(alienName string, possibleEmptyCities []string) *alien {
-    if w.aliens == nil {
-        w.aliens = make(map[string]*alien)
+// If alien doesn't exist, creates it in a random empty city and adds it to the world.
+// Returns pointer to alien with requested name.
+func (w *WorldX) CreateAlien(alienName string, possibleEmptyCities []string) *Alien {
+    if w.Aliens == nil {
+        w.Aliens = make(map[string]*Alien)
     }
 
-    if a, ok := w.aliens[alienName]; ok {
+    if a, ok := w.Aliens[alienName]; ok {
         return a
     } else {
         randomEmptyCity := w.getRandomEmptyCity(possibleEmptyCities)
-        newAlien := alien{
+        newAlien := Alien{
             name:      alienName,
             location:  randomEmptyCity,
-            isTrapped: randomEmptyCity.isIsolated(),
+            isTrapped: randomEmptyCity.IsIsolated(),
         }
-        w.aliens[alienName] = &newAlien
+        w.Aliens[alienName] = &newAlien
         randomEmptyCity.alien = &newAlien
         return &newAlien
     }
 }
 
-// Moves alien from its current city to a random connected city,
-// if an alien is already present they fight and both the city and aliens are destroyed
-func (w *WorldX) moveAlien(alien *alien) {
+// Moves alien from its current city to a random connected city if he isn't trapped,
+// if an alien is already present they fight and the city and both aliens are destroyed.
+func (w *WorldX) moveAlien(alien *Alien, writer *bufio.Writer) {
     if alien.isTrapped {
         return
     }
@@ -170,29 +188,41 @@ func (w *WorldX) moveAlien(alien *alien) {
         alien.isTrapped = true
         return
     } else if nextCity.alien != nil {
+        cityName, alien1Name, alien2Name := nextCity.name, alien.name, nextCity.alien.name
+        defer func() {
+            _, err := fmt.Fprintf(writer, "%s has been destroyed by alien %s and alien %s\n",
+                cityName, alien1Name, alien2Name)
+            if err != nil {
+                log.Panic(err)
+            }
+        }()
+
         w.destroyCity(nextCity, alien, nextCity.alien)
     } else {
         alien.location.alien = nil
         alien.location = nextCity
         nextCity.alien = alien
-        if nextCity.isIsolated() {
+        if nextCity.IsIsolated() {
             alien.isTrapped = true
         }
     }
 }
 
-// Removes connections to the city, and destroys the city and both aliens
-func (w *WorldX) destroyCity(city *city, alien1 *alien, alien2 *alien) {
-    defer fmt.Printf("%s has been destroyed by alien %s and alien %s\n", city.name, alien1.name, alien2.name)
+// Removes connections to the city, and destroys the city and both aliens.
+func (w *WorldX) destroyCity(city *City, alien1 *Alien, alien2 *Alien) {
     w.deleteAlien(alien1)
     w.deleteAlien(alien2)
     w.deleteCity(city)
 }
 
-func (w *WorldX) deleteCity(city *city) {
+func (w *WorldX) deleteCity(city *City) {
+    if city == nil {
+        return
+    }
+
     for dir, connection := range city.connectedCities {
         if connection != nil {
-            connection.connectedCities[direction(dir).getOpposite()] = nil
+            connection.connectedCities[Direction(dir).GetOpposite()] = nil
         }
     }
 
@@ -200,42 +230,70 @@ func (w *WorldX) deleteCity(city *city) {
         city.alien.location = nil
         city.alien = nil
     }
-    delete(w.cities, city.name)
+    delete(w.Cities, city.name)
     city = nil
 }
 
-func (w *WorldX) deleteAlien(alien *alien) {
+func (w *WorldX) deleteAlien(alien *Alien) {
+    if alien == nil {
+        return
+    }
+
     if alien.location != nil {
         alien.location.alien = nil
         alien.location = nil
     }
-    delete(w.aliens, alien.name)
+    delete(w.Aliens, alien.name)
     alien = nil
 }
 
-type alien struct {
+type Alien struct {
     name      string
-    location  *city
+    location  *City
     isTrapped bool
 }
 
-type city struct {
-    name            string
-    connectedCities [maxDirections]*city
-    alien           *alien
+func (a *Alien) Name() string {
+    return a.name
 }
 
-func (c *city) String() (cStr string) {
+func (a *Alien) Location() *City {
+    return a.location
+}
+
+func (a *Alien) IsTrapped() bool {
+    return a.isTrapped
+}
+
+type City struct {
+    name            string
+    connectedCities [MaxDirections]*City
+    alien           *Alien
+}
+
+func (c *City) Name() string {
+    return c.name
+}
+
+func (c *City) Connection(dir Direction) *City {
+    return c.connectedCities[dir]
+}
+
+func (c *City) Alien() *Alien {
+    return c.alien
+}
+
+func (c *City) String() (cStr string) {
     cStr = c.name
     for dir, connection := range c.connectedCities {
         if connection != nil {
-            cStr += " " + direction(dir).String() + "=" + connection.name
+            cStr += " " + Direction(dir).String() + "=" + connection.name
         }
     }
     return
 }
 
-func (c *city) isIsolated() bool {
+func (c *City) IsIsolated() bool {
     for _, connection := range c.connectedCities {
         if connection != nil {
             return false
@@ -244,67 +302,67 @@ func (c *city) isIsolated() bool {
     return true
 }
 
-// Returns pointer to random connected city or nil if city is isolated (i.e. doesn't have connections)
-func (c *city) getRandomConnection() (randomCity *city) {
-    if c.isIsolated() {
+// Returns pointer to random connected city or nil if city is isolated (i.e. doesn't have connections).
+func (c *City) getRandomConnection() (randomCity *City) {
+    if c.IsIsolated() {
         return nil
     }
 
     for {
-        if randomCity = c.connectedCities[rand.Intn(int(maxDirections))]; randomCity != nil {
+        if randomCity = c.connectedCities[rand.Intn(int(MaxDirections))]; randomCity != nil {
             return
         }
     }
 }
 
-type direction int
+type Direction int
 
 const (
-    north direction = iota
-    south
-    east
-    west
-    maxDirections
-    unknown         = -1
+    North Direction  = iota
+    South
+    East
+    West
+    MaxDirections
+    UnknownDirection = -1
 )
 
-func getDirection(dir string) direction {
+func GetDirection(dir string) Direction {
     switch dir {
     case "north":
-        return north
+        return North
     case "south":
-        return south
+        return South
     case "east":
-        return east
+        return East
     case "west":
-        return west
+        return West
     default:
-        return unknown
+        return UnknownDirection
     }
 }
 
-func (d direction) String() string {
-    if d < 0 || d >= maxDirections {
+func (d Direction) String() string {
+    if d < 0 || d >= MaxDirections {
         return "unknown"
     }
     return [...]string{"north", "south", "east", "west"}[d]
 }
 
-func (d direction) isValid() bool {
-    return d == north || d == south || d == east || d == west
+func (d Direction) IsValid() bool {
+    return d == North || d == South || d == East || d == West
 }
 
-func (d direction) getOpposite() direction {
+func (d Direction) GetOpposite() Direction {
     switch d {
-    case north:
-        return south
-    case south:
-        return north
-    case east:
-        return west
-    case west:
-        return east
+    case North:
+        return South
+    case South:
+        return North
+    case East:
+        return West
+    case West:
+        return East
     default:
-        return unknown
+        return UnknownDirection
     }
 }
